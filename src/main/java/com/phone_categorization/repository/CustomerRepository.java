a80601e68f7c4862df64b8737fb3f365adc9e4f8
khalidMahmoudBasuiny <khalid.mahmoud@asset.com.eg>
2021-08-30 01:07:37 +0200

    Build API #4 end categorization #3

--- src/main/java/com/phone_categorization/repository/CustomerRepository.java
+++ src/main/java/com/phone_categorization/repository/CustomerRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer,Integer>
+public interface CustomerRepository extends JpaRepository<Customer, Integer>
 {
-    public List<Customer> findAll();
+    List<Customer> findAll();
 }
