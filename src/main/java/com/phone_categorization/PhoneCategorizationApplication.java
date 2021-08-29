bbd51d90005064925c6ed67afc8f373ec7b4b14a
khalidMahmoudBasuiny <khalid.mahmoud@asset.com.eg>
2021-08-29 03:56:09 +0200

    Project creation

---
+++ src/main/java/com/phone_categorization/PhoneCategorizationApplication.java
@@ -0,0 +1,15 @@
+package com.phone_categorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneCategorizationApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PhoneCategorizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phone_categorization/PhoneCategorizationApplicationTests.java
@@ -0,0 +1,15 @@
+package com.phone_categorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneCategorizationApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
