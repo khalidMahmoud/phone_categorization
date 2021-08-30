7e0759df5fc8cae6c9d1b22e373cf247a3a2b4ff
khalidMahmoudBasuiny <khalid.mahmoud@asset.com.eg>
2021-08-30 03:41:19 +0200

    Build API #4 end categorization #4

--- src/main/java/com/phone_categorization/controller/PhoneController.java
+++ src/main/java/com/phone_categorization/controller/PhoneController.java
@@ -3,6 +3,7 @@ package com.phone_categorization.controller;
 import com.phone_categorization.entity.PhoneCategory;
 import com.phone_categorization.services.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class PhoneController
     @Autowired
     private CustomerServiceImpl customerService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "phoneList", method = RequestMethod.GET)
     public List<PhoneCategory> readCustomers()
     {
